e2090ad2faa8bba1c33aa71592f077a2f6a4c151
Vladyslav Sikalo <vladislavmediol@gmail.com>
2023-11-11 12:02:35 +0100

    initial files

---
+++ vite.config.js
@@ -0,0 +1,8 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  base: "/React-Registration-Form/",
+  plugins: [react()],
+})
